223affffd9b95faf610ed0cd50b7c08a10fd689d
Eirik Tobias Klebo <eirik@Eiriks-MBP.lan>
2023-03-04 09:26:26 +0100

    Legg til sidenav

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import { useState } from 'react';
-// import Sidenav from './components/Sidenav';
-
+import Sidenav from './components/sidenav';
 function App() {
 
 
   return (
     <div>
     <h1>All makt til eirik</h1>
-
+<Sidenav/>  
     </div>
   )
 }
