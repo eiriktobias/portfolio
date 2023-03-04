93d0a4c2f459eece2e1d63636f96e139b891ffb1
Eirik Tobias Klebo <eirik@Eiriks-MBP.lan>
2023-03-04 09:30:48 +0100

    test screen

--- src/components/sidenav.jsx
+++ src/components/sidenav.jsx
@@ -12,7 +12,7 @@ const Sidenav = () => {
         <AiOutlineMenu onClick={handleNav} className='absolute top-4 right-4 z-[99] md:hidden'/>
         {
             nav ? (
-                <div className='fixed w-full h-sceen bh-white/90 flex flex-col justify-center items-center z-20'>
+                <div className='fixed w-full h-screen bh-white/90 flex flex-col justify-center items-center z-20'>
                     <a href="#main" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
                         <AiOutlineHome size={20} />
                         <span className='pl-4'>Home</span>
